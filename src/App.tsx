189d1f68d60f6c5fbcd556b68231dcb4943a9577
vvasquez <vvasquez@leapit.com.do>
2024-10-30 20:26:43 -0400

    converting to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProductCard from "./components/ProductCard/ProductCard";
+
+interface Iproduct {
+  price: number;
+}
+
+const profucts: Array<Iproduct> = [{ price: 1.99 }];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <div className="flex">
+        <div style={{ width: "300px", height: "200px" }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: "300px", height: "200px" }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: "300px", height: "200px" }}>
+          <ProductCard />
+        </div>
+      </div>
+      <div className="flex">
+        <div style={{ width: "250px", height: "150px" }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: "250px", height: "150px" }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: "250px", height: "150px" }}>
+          <ProductCard />
+        </div>
+      </div>
+      <div className="flex">
+        <div style={{ width: "200px", height: "120px" }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: "200px", height: "120px" }}>
+          <ProductCard />
+        </div>
+        <div style={{ width: "200px", height: "120px" }}>
+          <ProductCard />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ProductCard.css";
+
+function ProductCard() {
+  return (
+    <div className="resize-container  w-full h-full">
+      <div className="product-card  p-4 w-full h-full rounded-lg border border-black">
+        Contenido
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
