dabb66c88875b3963af1b7f9d9d7ed189157a5e0
vjose1903 <vjose1903@outlook.es>
2024-10-31 14:22:43 -0400

    implementation ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,66 @@
 import "./App.css";
 import ProductCard from "./components/ProductCard/ProductCard";
 
-interface Iproduct {
+export interface Iproduct {
   price: number;
+  name: string;
+  description: string;
+  image: string;
+  condition?: string;
 }
 
-const profucts: Array<Iproduct> = [{ price: 1.99 }];
+const profucts: Array<Iproduct> = [
+  {
+    price: 1.99,
+    name: "Water Bottle",
+    description: "fresh mineral water bottle.",
+    image: `${process.env.PUBLIC_URL}/img/water-bottle.png`,
+    condition: undefined,
+  },
+  {
+    price: 5.99,
+    name: "Breast",
+    description: "Chicken breast with optional protein",
+    image: `${process.env.PUBLIC_URL}/img/breast.png`,
+    condition: "With club card. Limit 2 offer",
+  },
+  {
+    price: 1.99,
+    name: "Oil",
+    description: "Pure soybean oil.",
+    image: `${process.env.PUBLIC_URL}/img/oil.png`,
+    condition: undefined,
+  },
+];
+
+const rowStyles = [
+  { width: "300px", height: "200px" },
+  { width: "250px", height: "150px" },
+  { width: "170px", height: "120px" },
+];
 
 function App() {
   return (
-    <div className="flex flex-col">
-      <div className="flex">
-        <div style={{ width: "300px", height: "200px" }}>
-          <ProductCard />
-        </div>
-        <div style={{ width: "300px", height: "200px" }}>
-          <ProductCard />
-        </div>
-        <div style={{ width: "300px", height: "200px" }}>
-          <ProductCard />
-        </div>
-      </div>
-      <div className="flex">
-        <div style={{ width: "250px", height: "150px" }}>
-          <ProductCard />
-        </div>
-        <div style={{ width: "250px", height: "150px" }}>
-          <ProductCard />
-        </div>
-        <div style={{ width: "250px", height: "150px" }}>
-          <ProductCard />
-        </div>
-      </div>
-      <div className="flex">
-        <div style={{ width: "200px", height: "120px" }}>
-          <ProductCard />
-        </div>
-        <div style={{ width: "200px", height: "120px" }}>
-          <ProductCard />
-        </div>
-        <div style={{ width: "200px", height: "120px" }}>
-          <ProductCard />
-        </div>
-      </div>
+    <div className="flex flex-col gap-2 p-4">
+      {rowStyles.map((style) => {
+        return (
+          <div className="flex gap-2">
+            {profucts.map((product) => {
+              return (
+                <div style={style}>
+                  <ProductCard
+                    description={product.description}
+                    price={product.price}
+                    name={product.name}
+                    image={product.image}
+                    condition={product.condition}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "./ProductCard.css";
+import { Iproduct } from "../../App";
 
-function ProductCard() {
+const ProductCard: React.FC<Iproduct> = ({
+  name,
+  price,
+  description,
+  image,
+  condition,
+}) => {
+  let conditionDiv;
+
+  if (condition)
+    conditionDiv = <div className="condition-div">{condition}</div>;
+
   return (
-    <div className="resize-container  w-full h-full">
-      <div className="product-card  p-4 w-full h-full rounded-lg border border-black">
-        Contenido
+    <div className="resize-container  w-full h-full ">
+      <div className="w-full h-full product-card rounded-lg border border-black-10 shadow-md">
+        <img className="product-image h-full object-contain" src={image} />
+        <h2 className="product-name">{name}</h2>
+        <span className="product-description">{description}</span>
+        <p className="product-price">{price}</p>
+        {conditionDiv}
       </div>
     </div>
   );
-}
+};
 
 export default ProductCard;
